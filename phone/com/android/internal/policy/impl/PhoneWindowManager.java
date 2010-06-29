d771bc312ba0fb57b485b93a202fb4984796b7cc
cvpcs <root@cvpcs.org>
2010-06-29 01:25:55 -0500

    added volup/voldown longpress volume changer [cyanogen]
    Change-Id: Icd9b1c9953af27995dc2a5ca8d79ff1225da5a00

--- phone/com/android/internal/policy/impl/PhoneWindowManager.java
+++ phone/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -219,6 +219,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     
     int mPointerLocationMode = 0;
     PointerLocationView mPointerLocationView = null;
+    Long mTrackballHitTime;
+    boolean mVolumeUpPressed;
+    boolean mVolumeDownPressed;
+    static final long NEXT_DURATION = 400; 
     
     // The current size of the screen.
     int mW, mH;
@@ -466,6 +470,44 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             sendCloseSystemWindows(SYSTEM_DIALOG_REASON_RECENT_APPS);
             showRecentAppsDialog();
         }
+    };
+    
+    /**
+     * When a volumeup-key longpress expires, skip songs based on key press
+     */
+    Runnable mVolumeUpLongPress = new Runnable() {
+        public void run() {
+            /*
+             * Eat the longpress so it won't dismiss the recent apps dialog when
+             * the user lets go of the volume key
+             */
+            mVolumeUpPressed = false;
+            // Shamelessly copied from MediaPlaybackService.java, which
+            // should be public, but isn't.
+            Intent i = new Intent("com.android.music.musicservicecommand");
+            i.putExtra("command", "next");
+            
+            mContext.sendBroadcast(i);
+        };
+    };
+    
+    /**
+     * When a volumedown-key longpress expires, skip songs based on key press
+     */
+    Runnable mVolumeDownLongPress = new Runnable() {
+        public void run() {
+            /*
+             * Eat the longpress so it won't dismiss the recent apps dialog when
+             * the user lets go of the volume key
+             */
+            mVolumeDownPressed = false;
+            // Shamelessly copied from MediaPlaybackService.java, which
+            // should be public, but isn't.
+            Intent i = new Intent("com.android.music.musicservicecommand");
+            i.putExtra("command", "previous");
+            
+            mContext.sendBroadcast(i);
+        };
     };
 
     /**
@@ -1726,6 +1768,39 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
     
+    void handleVolumeKeyDown(int keycode) {
+        // when keyguard is showing and screen off, we need
+        // to handle the volume key for calls and music here
+        if (isInCall()) {
+            handleVolumeKey(AudioManager.STREAM_VOICE_CALL, keycode);
+        } 
+        // Take an initial hit time so we can decide for skip or volume adjust
+        else if (isMusicActive()) {
+            if (keycode == KeyEvent.KEYCODE_VOLUME_UP) {
+                mVolumeUpPressed = true;
+                mHandler.postDelayed(mVolumeUpLongPress, ViewConfiguration.getLongPressTimeout());
+            }
+            else {
+                mVolumeDownPressed = true;
+                mHandler.postDelayed(mVolumeDownLongPress, ViewConfiguration.getLongPressTimeout());
+            }
+        }
+    }
+    
+    void handleVolumeKeyUp(int keycode) {
+        if (isMusicActive()) {
+            
+            if (keycode == KeyEvent.KEYCODE_VOLUME_UP)
+                mHandler.removeCallbacks(mVolumeUpLongPress);
+            else
+                mHandler.removeCallbacks(mVolumeDownLongPress);
+            
+            // Normal volume change - not consumed be long press already
+            if (mVolumeUpPressed || mVolumeDownPressed)
+                handleVolumeKey(AudioManager.STREAM_MUSIC, keycode);
+        }
+    }
+    
     static boolean isMediaKey(int code) {
         if (code == KeyEvent.KEYCODE_HEADSETHOOK || 
                 code == KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE ||
@@ -1766,6 +1841,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
                 final boolean isKeyDown =
                         (event.type == RawInputEvent.EV_KEY) && (event.value != 0);
+
+		final boolean isKeyUp =
+			(event.type == RawInputEvent.EV_KEY) && (event.value == 0);
+
                 if (isWakeKey && isKeyDown) {
 
                     // tell the mediator about a wake key, it may decide to
@@ -1774,13 +1853,14 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     if (!mKeyguardMediator.onWakeKeyWhenKeyguardShowingTq(event.keycode)
                             && (event.keycode == KeyEvent.KEYCODE_VOLUME_DOWN
                                 || event.keycode == KeyEvent.KEYCODE_VOLUME_UP)) {
-                        // when keyguard is showing and screen off, we need
-                        // to handle the volume key for calls and  music here
-                        if (isInCall()) {
-                            handleVolumeKey(AudioManager.STREAM_VOICE_CALL, event.keycode);
-                        } else if (isMusicActive()) {
-                            handleVolumeKey(AudioManager.STREAM_MUSIC, event.keycode);
-                        }
+                        handleVolumeKeyDown(event.keycode);
+                    }
+                }
+                else if (isWakeKey && isKeyUp) {
+                    if (!mKeyguardMediator.onWakeKeyWhenKeyguardShowingTq(event.keycode)
+                            && (event.keycode == KeyEvent.KEYCODE_VOLUME_DOWN
+                                    || event.keycode == KeyEvent.KEYCODE_VOLUME_UP)) {
+                        handleVolumeKeyUp(event.keycode);
                     }
                 }
             }
